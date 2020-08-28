1ac9b1af51e19490131eefa28e2ad481a7c7b530
Gideon Mpungu <gmbugampungu@gmail.com>
2020-08-28 10:49:22 +0300

    Create sock_merchant.py

---
+++ sock_merchant.py
@@ -0,0 +1,32 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    x=[]
+    y=[]
+    ar.sort()
+    while len(ar)>1:
+        if ar[0]==ar[1]:
+            x.append(ar[:2])
+            del ar[:2]
+        else:
+            del ar[0]
+            
+    return len(x)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
